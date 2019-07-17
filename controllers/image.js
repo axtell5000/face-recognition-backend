cb6801c6531d64e0a26664e485229cf0d0e093ab
Stephen Axtell <axtell.stephen@gmail.com>
2019-07-17 16:27:01 +0200

    removing a lock file

--- controllers/image.js
+++ controllers/image.js
@@ -3,6 +3,7 @@ const Clarifai = require('clarifai');
 // initialize with your api key. This will also work in your browser via http://browserify.org/
 
 const app = new Clarifai.App({
+  //apiKey: process.env.API_CLARIFAI
   apiKey: process.env.API_CLARIFAI
 });
 
